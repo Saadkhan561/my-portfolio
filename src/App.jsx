86b19aa9dc6f49aea29179d8d8afc5e82b483d6a
Saad Khan <saadnadeem885@gmail.com>
2023-03-26 13:24:09 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <header>
-      <div className='header-main'>
-        <div className='header-subdiv'>
-          <div className='header-subdiv-item'>GITHUB</div>
-          <div className='header-subdiv-item'>LINKED-IN</div>
+      <div className="header-main">
+        <div className="header-subdiv">
+          <button className="moon">
+            <img src="./assets/moon.png" alt="" height={30} width={30}/>
+          </button>
+          <div className="header-subdiv-item">GITHUB</div>
+          <div className="header-subdiv-item">LINKED-IN</div>
         </div>
       </div>
-      <div className='intro-div'>
-        <div className='name-div'>
-          <h2>Hi...</h2>
+      <div className="intro-div">
+        <div className="name-div">
+          <h2 id="name-1">Hi...</h2>
           <br />
-          <h2>My name is Saad Nadeem Khan</h2>
+          <h2 id="name-2">Saad Here...</h2>
         </div>
-        <img className='profile-img'  src='./assets/profile-picture.jpg' alt=''/>
+        <img
+          className="profile-img"
+          src="./assets/profile-picture.jpg"
+          alt=""
+        />
         <div className="skill-div">
-          I'm a React JS Developer X Frontend Developer
+          <h2>I'm a React JS developer</h2>
         </div>
       </div>
     </header>
